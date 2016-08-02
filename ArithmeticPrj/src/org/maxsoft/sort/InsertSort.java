5d4cc3b0b182d993e62110db96e5df12d5731a7c
MaxSoft007 <zylwlh810@163.com>
2016-08-02 10:44:48 +0800

    BACKUP

--- ArithmeticPrj/src/org/maxsoft/sort/InsertSort.java
+++ ArithmeticPrj/src/org/maxsoft/sort/InsertSort.java
@@ -24,10 +24,21 @@ public class InsertSort {
 		}
 	}
 	
+	public void insertSort2(int[] a){
+		int size = a.length;
+	    for (int i=0; i<size; i++){
+	        for (int j=i; j>0 && a[j-1] > a[j]; j--){
+			    int t = a[j];
+				a[j] = a[j-1];
+				a[j-1] = t;
+	        }
+	    }
+	}
+	
 	public static void main(String[] args){
 		int[] sort = {3,8,6,4,2,1,0,5,9,7};
 		InsertSort insertSort = new InsertSort();
-		insertSort.insertSort(sort);
+		insertSort.insertSort2(sort);
 		for (int i = 0; i < sort.length; i++) {
 			System.out.print(" "+sort[i]+" ");
 		}
